b58ea8e69054fa0e68a05554ad006c553cefc0d6
Siddharth Sinha <outflanker@users.noreply.github.com>
2015-12-06 01:45:06 -0500

    Update search.php

--- Web/search.php
+++ Web/search.php
@@ -1,13 +1,27 @@
-		<div class="col-lg-12" id="search-col">
-			<div class="jumbotron">
-				<h1>Search</h1>
-				<p class="lead">Enter your search query</p>
-				<div class="input-group">
-				  <input type="text" class="form-control" placeholder="Search for..." id="query">
-				  <span class="input-group-btn">
-				  <button class="btn btn-success" type="button" id="submit">Go!</button>
-				  </span>
-				</div>
-			</div>
-		</div>
-		<div id="search-container"></div>
+   <div class="bodyContainer">
+       <div class="col-lg-12" id="search-col">
+           <div class="jumbotron">
+               <div class="container">
+                   <div class="row">                        
+                       <div class="col-lg-4">
+                           <div style="float:left">
+                               <img src="Logo.png" />
+                           </div>
+                           <div style="float:right">
+                               <h1>Aha!</h1>
+                           </div>
+                       </div>
+                   </div>
+                   <p class="lead">Enter your search query</p>
+                   <div class="input-group">
+                       <input type="text" class="form-control" placeholder="Search for..." id="query">
+                       <span class="input-group-btn">
+                           <button class="btn btn-success" type="submit" id="submit">Go!</button>
+                       </span>
+                   </div>
+               </div>            
+           </div>
+       </div>
+   </div>
+   <div id="search-container"></div>
+
