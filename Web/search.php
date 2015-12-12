05352467589c71818809d8b6467128a8bc8eeb31
farhahuma <farhahuma@gmail.com>
2015-12-12 16:56:04 -0500

    Display search query after click

--- Web/search.php
+++ Web/search.php
@@ -1,27 +1,39 @@
    <div class="bodyContainer">
-       <div class="col-lg-12" id="search-col">
+       <div class="col-lg-12" id="search-col">	   
            <div class="jumbotron">
                <div class="container">
                    <div class="row">                        
                        <div class="col-lg-4">
                            <div style="float:left">
                                <img src="Logo.png" />
-                           </div>
-                           <div style="float:right">
-                               <h1>Aha!</h1>
-                           </div>
+                           </div>                           
                        </div>
-                   </div>
-                   <p class="lead">Enter your search query</p>
+                   </div>                   
                    <div class="input-group">
-                       <input type="text" class="form-control" placeholder="Search for..." id="query">
+                       <input type="text" class="form-control query" placeholder="Search for..." id="query">
                        <span class="input-group-btn">
-                           <button class="btn btn-success" type="submit" id="submit">Go!</button>
+                           <button class="btn btn-success submit" type="submit">Go!</button>
                        </span>
                    </div>
                </div>            
            </div>
        </div>
+	    <div class="row" id="search-col-small" style="display:none">
+            <div class="col-lg-2"></div>
+            <div class="col-lg-1">
+                <div style="float:right">
+                    <img src="LogoSmall.png" />
+                </div>
+            </div>
+            <div class="col-lg-6">
+                <div class="input-group">
+                    <input type="text" class="form-control query" placeholder="Search for..." id="querySmall">
+                    <span class="input-group-btn">
+                        <button class="btn btn-success submit" type="submit">Go!</button>
+                    </span>
+                </div>
+            </div>
+        </div>
    </div>
    <div id="search-container"></div>
 
