ce48bd03c57415b708eaed4fb9e9db6782f7aaee
farhahuma <farhahuma@gmail.com>
2015-12-12 16:54:30 -0500

    Storing facet information to cookie

--- Web/js/action.js
+++ Web/js/action.js
@@ -1,21 +1,26 @@
 $(document).ready(function(){	
 	function search(id){		
-		var query = $("#"+id).val();		
-		document.cookie= query;		
-		var query="http://10.84.17.48:8983/solr/project/select?q.alt=text_en:"+query;
+		var query = $("#"+id).val();
+		$("#querySmall").val(query);		
+		document.cookie = "text_en:"+query;		
+		var query="http://ufkk59b06f3a.farhahum.koding.io:8983/solr/project/select?q.alt=text_en:"+query;
 		$("#search-col").hide();
-		$("#search-col-small").show();
+		$("#search-col-small").show();		
 		$.post('action.php', {query: query}, function(data){
 			$('#search-container').html(data);
-		});
+		});		
 	}
 	function searchtags(query,isHashTag){
-		document.cookie= document.cookie + query;		
-		if(isHashTag)
-			var query="http://10.84.17.48:8983/solr/project/select?wt=json&indent=true&defType=dismax&q.alt=tweet_hashtags:"+query;
-		else
-			var query="http://10.84.17.48:8983/solr/project/select?wt=json&indent=true&defType=dismax&q.alt=content_tags:"+query;
-		
+		document.cookie= query;		
+		if(isHashTag){
+			document.cookie = "tweet_hashtags:"+query;
+			var query="http://ufkk59b06f3a.farhahum.koding.io:8983/:8983/solr/project/select?wt=json&indent=true&defType=dismax&q.alt=tweet_hashtags:"+query;
+		}
+		else{
+			document.cookie = "content_tags:"+query;
+			var query="http://ufkk59b06f3a.farhahum.koding.io:8983/solr/project/select?wt=json&indent=true&defType=dismax&q.alt=content_tags:"+query;
+		}
+		alert(document.cookie);
 		$.post('action.php', {query: query}, function(data){
 			$('#search-container').html(data);
 		});
@@ -56,4 +61,39 @@ $(document).ready(function(){
 		e.preventDefault();
 		searchtags($(this).text(),false);
 	})
+	
+	$("#search-container").on("click",".facet",function(e){
+		var dict = {
+		"Language" : "lang",
+		"English" : "en",
+		"German" : "de",
+		"Russian" : "ru",
+		"Spanish" : "es",
+		"French" : "fr",
+		"Arabic" : "ar",
+		"Tweet Hashtags" :"tweet_hashtags",
+		"Content Tags":"content_tags",
+		"Location" : "location"
+		};
+		
+		var facetName = $(this).text();
+		var facet = facetName.substring(0, facetName.indexOf('('));
+		var facetHeader = $(this).parent().parent().parent().children(":first").text();			
+		
+		if(typeof (dict[facet])!='undefined')
+			facet = dict[facet];
+		if(typeof (dict[facetHeader])!='undefined')
+			facetHeader = dict[facetHeader];
+		
+		e.preventDefault();
+		document.cookie = document.cookie +", "+ facetHeader + ":" + facet;		
+		alert(document.cookie);
+	})
+	
+});
+$body = $("body");
+
+$(document).on({
+    ajaxStart: function() { $body.addClass("loading");    },
+     ajaxStop: function() { $body.removeClass("loading"); }    
 });
