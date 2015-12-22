9bbb5c8244881fd1e6e7506f2c7729d8b8bb6403
farhahuma <farhahuma@gmail.com>
2015-12-22 00:49:14 -0500

    Update about.php

--- Web/about.php
+++ Web/about.php
@@ -1,15 +1,75 @@
-<h2><p>Welcome to AHA!</p></h2>
+<div class="container">
+        <div class="row">                        
+            <div class="col-lg-1">
+                <div style="float:left">
+                    <img src="LogoSmall.png" />
+                </div>                           
+            </div>                          
+			<div class="col-lg-6">
+					<div class="page-header">
+						<h1>Aha! <small>What we're all about</small></h1>
+					</div>                    
+			</div>
+		</div>
+</div>    
 
-<h3><p>Aha! is a multilingual faceted search system that allows the user to browse tweets in six different languages. The language of the query is detected automatically and the most recent, relevant tweets are returned as results. The results can be filtered based on the user's selection of the facets shown. The user can enter a query in any one of the 6 languages: English, French, Arabic, German, Russian, Spanish, and get the most relevant results. The results are ranked based on the relevancy.
-</p></h3>
-<h3><p>Aha! striving to serve you better...</p></h3>
+<p>Aha! is a multilingual and faceted search system that allows the user to search a repository of twitter data </p>
+<p>The user can enter a query in any one of the 6 languages: English, French, Arabic, German, Russian, Spanish to get the most relevant results.</p>
+<p></p>
+<p>The results can be filtered based on the user's selection of the facets shown. </p>
 
-<h3>
-<p>Created by:</p>
-<p>Abhinav Chaturvedi</br>
-Abhinav Sharma</br>
-Archana Krishnan</br>
-Farha Huma</br>
-Priyanka Govindraj</br>
-Siddharth Krishna Sinha</p>
-</h3>
+<div class="alert alert-info" role="alert">
+  The Team
+</div>
+<div class="row">
+  <div class="col-sm-6 col-md-4">
+    <div class="thumbnail">
+      <span class="glyphicon glyphicon-tree-deciduous"></span>
+      <div class="caption">
+        <h4>Abhinav Chaturvedi</h4>        
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-6 col-md-4">
+    <div class="thumbnail">
+      <span class="glyphicon glyphicon-tree-conifer"></span>
+      <div class="caption">
+        <h4>Abhinav Sharma</h4>        
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-6 col-md-4">
+    <div class="thumbnail">
+      <span class="glyphicon glyphicon-knight"></span>
+      <div class="caption">
+        <h4>Archana Krishnan</h4>        
+      </div>
+    </div>
+  </div>
+</div>
+<div class="row">
+  <div class="col-sm-6 col-md-4">
+    <div class="thumbnail">
+      <span class="glyphicon glyphicon-bishop"></span>
+      <div class="caption">
+        <h4>Farha Huma</h4>        
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-6 col-md-4">
+    <div class="thumbnail">
+      <span class="glyphicon glyphicon-grain"></span>
+      <div class="caption">
+        <h4>Priyanka Govindraj</h4>        
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-6 col-md-4">
+    <div class="thumbnail">
+      <span class="glyphicon glyphicon-send"></span>
+      <div class="caption">
+        <h4>Siddharth Krishna Sinha</h4>        
+      </div>
+    </div>
+  </div>
+</div>
